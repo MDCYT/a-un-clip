566cbfdfb4b7a225cc79f8805636a1bdd6d78ee8
MDCDEV <me@mdcdev.me>
2022-11-23 09:32:29 -0500

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,8 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD
+    password: process.env.MYSQL_PASSWORD,
+    port: process.env.MYSQL_PORT,
 });
 
 connection.connect((err) => {
